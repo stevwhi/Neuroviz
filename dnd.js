1e1ffd44e05a5ef1fe996133d2026bcd78f7e2e6
whitbys <99200921+whitbys@users.noreply.github.com>
2023-10-20 20:05:36 +0100

    3d brain model found

--- dnd.js
+++ dnd.js
@@ -1,29 +1,34 @@
-const dropArea = document.getElementById('upload-area');
 const fileInput = document.getElementById('fileInput');
+const dropZone = document.getElementById('uploadArea');
 
-console.log("hello");
+// Prevent default behaviour
+['drop', 'dragleave', 'dragover', 'dragenter'].forEach(event => {
 
-// Prevent default drag behaviors
-['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-    dropArea.addEventListener(eventName, preventDefaults);
-    document.body.addEventListener(eventName, preventDefaults);
+    dropZone.addEventListener(event, preventDefaults);
+    document.body.addEventListener(event, preventDefaults);
 });
 
-// Highlight drop area when item is dragged over it
-['dragenter', 'dragover'].forEach(eventName => {
-    dropArea.addEventListener(eventName, highlight);
+// Highlighting
+['dragover', 'dragenter'].forEach(event => {
+
+    dropZone.addEventListener(event, highlight);
 });
 
-['dragleave', 'drop'].forEach(eventName => {
-    dropArea.addEventListener(eventName, unhighlight);
+['drop', 'dragleave'].forEach(event => {
+
+    dropZone.addEventListener(event, unhighlight);
 });
 
-// Handle dropped files
-dropArea.addEventListener('drop', handleDrop);
+// File handling
+dropZone.addEventListener('drop', handleFileDrop);
 
-// If someone wants to click to upload instead
-dropArea.addEventListener('click', () => fileInput.click());
-fileInput.addEventListener('change', handleFiles);
+// click upload option
+dropZone.addEventListener('click', () => fileInput.click());
+fileInput.addEventListener('change', handleFile);
+
+
+
+//functions -------------------------------------------------
 
 function preventDefaults(e) {
     e.preventDefault();
@@ -31,18 +36,18 @@ function preventDefaults(e) {
 }
 
 function highlight() {
-    dropArea.style.backgroundColor = '#f0f0f0';
+    dropZone.style.backgroundColor = '#f0f0f1';
 }
 
 function unhighlight() {
-    dropArea.style.backgroundColor = '#fff';
+    dropZone.style.backgroundColor = '#ffd';
 }
 
-function handleDrop(e) {
-    let files = e.dataTransfer.files;
-    handleFiles(files);
+function handleFileDrop(e) {
+    let file = e.dataTransfer.files;
+    handleFile(file);
 }
 
-function handleFiles(files) {
-    console.log(files); // For now, just log the files
+function handleFile(file) {
+    console.log(file); //temporary method
 }
--- scene.js
+++ scene.js
@@ -1,40 +1,53 @@
+import { Scene, PerspectiveCamera, WebGLRenderer, AmbientLight } from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-//set scene
+
+//create scene
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;  // Set camera distance from the object
-
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Add window resize event listener
-window.addEventListener('resize', () => {
-    const newWidth = window.innerWidth;
-    const newHeight = window.innerHeight;
-    
-    renderer.setSize(newWidth, newHeight);
-    camera.aspect = newWidth / newHeight;
-    camera.updateProjectionMatrix();
-});
+window.addEventListener('resize', windowResize, false);
 
-//load brain model
+camera.position.z = 5;
 
-const loader = new THREE.GLTFLoader();
+const light = new THREE.AmbientLight(0xffffff, 0.85); 
+scene.add(light);
 
-loader.load(
-    'path_to_brain_model.gltf',  // Replace with your model's path
-    function(gltf) {
+
+// Load Brain
+const gltfLoader = new GLTFLoader();
+gltfLoader.load(
+    '/public/brain/scene.gltf', 
+    function (gltf) {
         scene.add(gltf.scene);
-        animate();  // Start the rendering loop after loading the model
+        animate(); 
     },
-    undefined,
-    function(error) {
-        console.error("An error occurred while loading the model:", error);
+    undefined, 
+    function (error) {
+        console.error('An error happened', error);
     }
-);
+    );
+
+
+//functions--------------------------------------------------------
+
+function windowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
 function animate() {
     requestAnimationFrame(animate);
+
+    // controls.update();
+
     renderer.render(scene, camera);
 }
+
+
+
