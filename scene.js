58defe9d72e9f4507941978b00b70a39111e86b1
whitbys <99200921+whitbys@users.noreply.github.com>
2023-11-03 22:55:58 +0000

    better brain loaded

--- scene.js
+++ scene.js
@@ -1,54 +1,56 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 
-//create scene
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+let scene, camera, renderer, controls;
 
-window.addEventListener('resize', windowResize, false);
+init();
+animate();
 
-camera.position.z = 5;
+function init() {
+    scene = new THREE.Scene();
 
-const light = new THREE.AmbientLight(0xffffff, 0.85); 
-scene.add(light);
+    //camera
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
 
-
-// Load Brain
-const gltfLoader = new GLTFLoader();
-gltfLoader.load(
-    '/public/brain/scene.gltf', 
-    function (gltf) {
-        scene.add(gltf.scene);
-        animate(); 
-    },
-    undefined, 
-    function (error) {
-        console.error('An error happened', error);
-    }
-    );
-
-
-
-
-//functions--------------------------------------------------------
-
-function windowResize() {
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+    //renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    // Handling window resize
+    window.addEventListener('resize', () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    });
+
+    // Create a directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+
+// Set the position of the light (adjust the values as needed)
+directionalLight.position.set(0, 1, 0);
+
+// Add the light to the scene
+scene.add(directionalLight);
+    //brain
+    const objLoader = new OBJLoader();
+        objLoader.load('/public/Brain/obj/Brain1.obj', function (object) {
+            object.position.set(0, 0, 0);
+            object.scale.set(10, 10, 10); 
+            scene.add(object);
+
+        });
+
+    //interactivity
+    controls = new OrbitControls(camera, renderer.domElement);
 }
 
 function animate() {
     requestAnimationFrame(animate);
-
-    // controls.update();
-
+    controls.update();
     renderer.render(scene, camera);
 }
-
 
 
